1ec6d4f3472750cf10d7a8d1e767351f6278166b
iok206 <iok206@nyu.edu>
2023-09-18 21:44:38 -0400

    First commit

---
+++ HW0.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    
+    for (int i = 1; i <= 100; i++){
+
+      if(i != 8){
+
+        System.out.println(i);
+        
+      }
+      
+    }
+    
+  }
+}
